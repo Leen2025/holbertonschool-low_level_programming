e5e748e1d611dbdf75c88417045f7bf46bc2228e
root <leeeen4@hotmail.com>
2025-02-12 21:28:30 +0000

    6-size.c

--- hello_world/6-size.c
+++ hello_world/6-size.c
@@ -3,7 +3,6 @@
  * main - A program that prints the size of various computer types
  * Return 0 (Success)
  */
-
 int main(void)
 {
 	char a;
@@ -12,15 +11,10 @@ int main(void)
 	long long int d;
 	float f;
 
-printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)
-sizeof(a));
-printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)
-sizeof(b));
-printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)
-sizeof(c));
-printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)
-sizeof(d));
-printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)
-sizeof(f));
+printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)sizeof(a));
+printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)sizeof(b));
+printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)sizeof(c));
+printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)sizeof(d));
+printf("Size of a char: %lu 1 byte(s)\n",(unsigned long)sizeof(f));
 return (0);
 }

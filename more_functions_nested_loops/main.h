0cc0770000b33be0390033eebc267f4f06211729
Leen2025 <leeeen4@hotmail.com>
2025-02-26 18:01:28 +0000

    10-print_triangle.c

--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,5 +1,5 @@
 #ifndef MAIN_H
 #define MAIN_H
 int _putchar(char c);
-void print_square(int size);/* Function prototype */
+void print_triangle(int size);/* Function prototype */
 #endif /* MAIN_H */

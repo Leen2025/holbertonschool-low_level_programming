325364f6eb2c9cdc0b93a280792e38fefa597d82
Leen2025 <leeeen4@hotmail.com>
2025-03-03 10:34:17 +0000

    8-print_diagsums.c

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
 #include "main.h"
 /**
-* print_diagsums -Entry point
-* @a: input
-* @size: input
-* Return: Always 0 (Success)
+* print_diagsums - Prints the sum of the two diagonals of a square matrix.
+* @a: Pointer to the first element of the square matrix.
+* @size: The size of the matrix (number of rows/columns).
 */
 void print_diagsums(int *a, int size)
 {
-int i, n, sum1 = 0, sum2 = 0;
-for (i = 0; i <= (size * size); i = i + size + 1)
-sum1 = sum1 + a[i];
-for (n = size - 1; n <= (size * size) - size; n = n + size - 1)
-sum2 = sum2 + a[n];
-printf("%d, %d", sum1, sum2);
+int i, sum1 = 0, sum2 = 0;
+for (i = 0; i < size; i++)
+{
+sum1 += a[i * size + i];
+sum2 += a[i * size + (size - 1 - i)];
+}
+printf("%d, %d\n", sum1, sum2);
 }

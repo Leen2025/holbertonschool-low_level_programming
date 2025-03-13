f285d2e374232e1604fe1da86f5c2229fa125062
Leen2025 <leeeen4@hotmail.com>
2025-03-13 16:17:36 +0000

    1-string_nconcat.c

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -2,18 +2,19 @@
 #include <string.h>
 #include "main.h"
 /**
- * string_nconcat - Concatenates two strings.
- * @s1: The first string.
- * @s2: The second string.
- * @n: The number of bytes to copy from s2.
+ * string_nconcat - Concatenates two strings up to n bytes of s2
+ * @s1: The first string
+ * @s2: The second string
+ * @n: The maximum number of bytes from s2 to concatenate
  *
- * Return: Pointer to newly allocated space containing s1 followed by
- * first n bytes of s2, or NULL on failure.
+ * Return: Pointer to newly allocated space containing s1 +
+ * first n bytes of s2
+ *         NULL if memory allocation fails
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
+char *concat;
 unsigned int len1, len2, i, j;
-char *result;
 if (s1 == NULL)
 s1 = "";
 if (s2 == NULL)
@@ -22,13 +23,13 @@ len1 = strlen(s1);
 len2 = strlen(s2);
 if (n >= len2)
 n = len2;
-result = malloc(len1 + n + 1);
-if (result == NULL)
+concat = malloc(sizeof(char) * (len1 + n + 1));
+if (concat == NULL)
 return (NULL);
 for (i = 0; i < len1; i++)
-result[i] = s1[i];
-for (j = 0; j < n; j++, i++)
-result[i] = s2[j];
-result[i] = '\0';
-return (result);
+concat[i] = s1[i];
+for (j = 0; j < n; j++)
+concat[i + j] = s2[j];
+concat[i + j] = '\0';
+return (concat);
 }

fd2629af865da1582e238ca24d5759831a5a31e8
Leen2025 <leeeen4@hotmail.com>
2025-03-17 09:57:04 +0000

    2-int_index.c

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -1,4 +1,4 @@
 #ifndef FUNCTION_POINTERS_H
 #define FUNCTION_POINTERS_H
-void array_iterator(int *array, size_t size, void (*action)(int));
+int int_index(int *array, int size, int (*cmp)(int));
 #endif

1a2471c684158bbac745a81ad1adcc87aabf45b9
Leen2025 <leeeen4@hotmail.com>
2025-04-01 05:19:04 +0000

    4-free_list.c

---
+++ singly_linked_lists/4-free_list.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "lists.h"
+/**
+ * free_list -Function that frees a singly linked list.
+ * @head: Pointer to singly linked list.
+ *
+ * Return: no return.
+ *
+ */
+void free_list(list_t *head)
+{
+list_t *temp_variable;
+temp_variable = head;
+while (head != NULL)
+{
+temp_variable = head; 
+head = head->next;
+free(temp_variable);
+}
+free(head);
+}

5fa6b3064b89f20b4a6e422e8537cfb4d23d5334
Leen2025 <leeeen4@hotmail.com>
2025-04-03 12:52:45 +0000

    6-sum_dlistint.c

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * sum_dlistint - function that return the sum of all the , 
+ * sum_dlistint - function that return the sum of all the ,
  * data (n) of a linked list.
  * @head: pointer to pointer to head of linked list.
  *
